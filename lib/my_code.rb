c5014fdef901de4c29df5ff6a7c447f7260d6d1f
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-08 01:52:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,60 @@
 # My Code here....
+def map_to_negativize(source_array)
+  map_array = []
+  i = 0
+  while i < source_array.length do
+    map_array.push(source_array[i] * -1)
+    i += 1
+  end
+  return map_array
+end
+
+def map_to_no_change(source_array)
+  map_array =[]
+  i = 0
+  while i < source_array.length do
+    map_array.push(source_array[i])
+    i += 1
+  end
+  return map_array
+end
+
+def map_to_double(source_array)
+  map_array = []
+  i = 0
+  while i < source_array.length do
+    map_array.push(source_array[i] * 2)
+    i += 1
+  end
+  return map_array
+end
+
+def map_to_square(source_array)
+  map_array = []
+  i = 0
+  while i < source_array.length do
+    map_array.push(source_array[i] * source_array[i])
+    i += 1
+  end
+  return map_array
+end
+
+def reduce_to_total(source_array, starting_point)
+  total = 0
+  i = 0
+  while i < source_array.length do
+    total += source_array[i]
+    i += 1
+  end
+  return total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
+end
+
